b6b4b53a23919ed070e07095f84b471d10a9914c
hope0312 <69333932+hope0312@users.noreply.github.com>
2021-03-10 15:38:31 +0800

    0310

--- indexjs.js
+++ indexjs.js
@@ -261,3 +261,76 @@ function setTable(top5, probs)
 
 }
 
+
+
+
+
+
+
+//get the latex symbols by indices 
+function getSymbols(indices)
+{
+    var outp = []
+    for (var i= 0 ; i < indices.length ; i++)
+        outp[i] = symbols[indices[i]]
+    return outp
+}
+	  
+//load the class names 	  
+async function loadDict()
+{
+  await $.ajax({
+  url: 'class_names.txt',
+  dataType: 'text',}).done(success);
+}
+	  
+//load the class names 
+function success(data)
+{
+    lst = data.split(/\n/)
+    symbols = []
+    for(var i = 0 ; i < lst.length -1  ; i++)
+    {
+        let symbol = lst[i]
+        symbols[i] = symbol
+    }
+}
+	  
+//get indices of the top probs
+function findIndicesOfMax(inp, count) {
+    var outp = [];
+    for (var i = 0; i < inp.length; i++) {
+        outp.push(i); // add index to output array
+        if (outp.length > count) {
+            outp.sort(function(a, b) { return inp[b] - inp[a]; }); // descending sort the output array
+            outp.pop(); // remove the last index (index of smallest element in output array)
+        }
+    }
+    return outp;
+}
+	  
+//find the top 5 predictions 
+function findTopValues(inp, count){
+    var outp = [];
+    let indices = findIndicesOfMax(inp, count)
+    // show 5 greatest scores
+    for (var i = 0; i < indices.length; i++)
+        outp[i] = inp[indices[i]]
+    return outp
+}
+	 
+//preprocess the data 
+function preprocess(imgData)
+{
+return tf.tidy(()=>{
+	  
+    const tensor = tf.fromPixels(imgData).toFloat()
+    const offset = tf.scalar(255.0);
+    // Normalize the image 
+    const normalized = tf.scalar(1.0).sub(tensor.div(offset));
+    const resized = tf.image.resizeBilinear(normalized, [28, 28])
+    const sliced   = resized.slice([0, 0, 1], [28, 28, 1])
+    const batched = sliced.expandDims(0)
+    return batched
+})
+}
